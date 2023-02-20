db9f5a4cf296105036c5fb9b0ec0e4e15dbc0eb7
hasan1297 <mahamudulhasan1297@gmail.comm>
2023-02-20 19:37:23 +0600

    header and footer

---
+++ partials/_footer.php
@@ -0,0 +1,160 @@
+<div class="footer">
+
+  <div class="footer-content">
+    <div class="p-2 footer-column about-us">
+      <div class="footer-head header-font">
+        About
+      </div>
+      <div class="footer-item">
+        <a href="/en/about_us">About Us</a>
+      </div>
+      <div class="footer-item">
+        <a href="/career?locale=en">Career</a>
+      </div>
+      <div class="footer-item">
+        <a href="/en/gallery_albums">Gallery</a>
+      </div>
+      <div class="footer-item">
+        <a href="/faqs?locale=en">FAQ</a>
+      </div>
+    </div>
+    <div class="p-2 footer-column legals">
+      <div class="footer-head">
+        Legal
+      </div>
+      <div class="footer-item">
+        <a href="/en/privacy_policy">Privacy Policy</a>
+      </div>
+      <div class="footer-item">
+        <a href="/en/terms_of_services">Terms of Services</a>
+      </div>
+      <div class="footer-item">
+        <a href="/en/risk_management">Risk Management</a>
+      </div>
+    </div>
+    <div class="p-2 footer-column contact-us">
+      <div class="footer-head">
+        Contact
+      </div>
+      <div class="footer-item">
+        <p>
+          <span>Email: </span>
+          <a href="mailto:info@ifarmer.asia">
+            info@ifarmer.asia
+          </a>
+        </p>
+      </div>
+      <div class="footer-item">
+        <p><span>For Call: </span>
+          <a href="tel:+8801302536026">
+            +8801302536026
+          </a>
+        </p>
+      </div>
+      <div class="footer-item">
+        <p><span>Calling Hours: </span>Sat -Thurs, 10am-6pm</p>
+      </div>
+      <div class="footer-item">
+        <p><span>For Whatsapp: </span>
+          <a href="tel:+8801302536026">
+            +8801784167973
+          </a>
+        </p>
+      </div>
+      <div class="footer-item">
+        <p><span>Singapore Address: </span>20A Tanjong Pagar Road, Singapore (088443)</p>
+      </div>
+      <div class="footer-item">
+        <p><span>Bangladesh Address: </span>House - 8E, Road - 81, Gulshan 2, Dhaka 1212</p>
+      </div>
+      <div class="footer-item">
+        <p><span>Visiting Hours: </span>Sun-Thurs,</p>
+        (appointment basis)
+      </div>
+      <div class="footer-item">
+        <div class="footer-social py-2">
+          <div class="social-insta">
+            <a href="https://www.instagram.com/ifarmerasia/" target="_blank">
+              <i class="fa fa-instagram gtm-instagram"></i>
+            </a>
+          </div>
+          <div class="social-twitter">
+            <a href="https://twitter.com/iFarmer_Asia" target="_blank">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </div>
+          <div class="social-fb">
+            <a href="https://www.facebook.com/ifarmerasia/" target="_blank">
+              <i class="fa fa-facebook-f gtm-facebook"></i>
+            </a>
+          </div>
+          <div class="social-whatsapp">
+            <a href="https://cutt.ly/QJRNzl4" target="_blank">
+              <i class="fa fa-whatsapp gtm-whatsapp"></i>
+            </a>                                                                              
+          </div>
+          <div class="social-linkedin">
+            <a href="https://www.linkedin.com/company/ifarmerasia/" target="_blank">
+              <i class="fa fa-linkedin gtm-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="p-2 app-links-container footer-column">
+      <div class="footer-head">
+        Our App
+      </div>
+      <div class="footer-item">
+        <div class="footer-btn">
+          <a href="https://rebrand.ly/iFarmerApp" target="_blank">
+            <button class="btn-android"></button>
+          </a>
+        </div>
+        <div class="footer-btn">
+          <a href="https://rebrand.ly/iFarmerIOS-App" target="_blank">
+            <button class="btn-ios"></button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="p-2 newsletter-container footer-column">
+        <div class="footer-head">
+          Stay in the loop
+        </div>
+        <div class="footer-item">
+          <div class="footer-subscription">
+            <form class="new_email_sub" id="new_email_sub" action="/email_subs?locale=en" accept-charset="UTF-8" data-remote="true" method="post"><input name="utf8" type="hidden" value="✓">
+              <input id="emailSub" placeholder="Email*" required="required" type="text" name="email_sub[email]">
+              <button type="submit" data-disable="true">
+                <i class="fa fa-paper-plane gtm-email"></i>
+              </button>
+</form>          </div>
+        </div>
+        <div class="footer-item">
+          <div class="footer-sub-msg">
+            Stay in touch with us for the freshest products!
+          </div>
+        </div>
+      </div>
+    <div class="p-2 footer-column company-info">
+      <div class="footer-head">
+        Company Information 
+      </div>
+      <div class="footer-item">
+        Trade License Number: 273687
+
+      </div>
+      <div class="footer-item">
+        BIN Number: 0017302330402
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="copyright">
+    <div class="copyright-msg pb-5 pt-1 px-2">
+        Copyright © 2019-2023 iFarmer, All Rights Reserved. Call us: <a href="tel:+8801302536026">+8801302536026</a> or send us a mail: <a href="mailto:info@ifarmer.asia">info@ifarmer.asia</a>
+    </div>
+  </div>
+
+</div>
